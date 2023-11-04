2891beaa21b94c0e60a4b60ecf41573df287e446
Eduardo Moura <emoufilho@gmail.com>
2023-11-04 13:20:47 -0300

    aula - secao 6

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -3,8 +3,14 @@ import { Component, OnInit } from '@angular/core';
 @Component({
   selector: 'app-root',
   template: `
-  <app-data-biding></app-data-biding>
-  <router-outlet></router-outlet>`
+  <!--
+  <app-diretivas-atributos>
+    <h1>Aulas de Diretivas de Atributo - ng-content </h1>
+    <h3>Final da Aula - ng-content </h3>
+  </app-diretivas-atributos>
+  <router-outlet></router-outlet>
+  -->
+  <app-diretivas-atributos></app-diretivas-atributos>`
 })
 
 export class AppComponent implements OnInit {
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,19 +3,25 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { TitleComponent } from './title/title.component';
-import { DataBidingComponent } from './data-biding/data-biding.component';
+import { CommonModule } from '@angular/common';
 import { FormsModule } from '@angular/forms';
+import { DataBidingComponent } from './data-biding/data-biding.component';
+import { DiretivasEstruturaisComponent } from './diretivas-estruturais/diretivas-estruturais.component';
+import { DiretivasAtributosComponent } from './diretivas-atributos/diretivas-atributos.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     TitleComponent,
-    DataBidingComponent
+    DataBidingComponent,
+    DiretivasEstruturaisComponent,
+    DiretivasAtributosComponent
   ],
   imports: [
     BrowserModule,
     AppRoutingModule,
-    FormsModule
+    FormsModule,
+    CommonModule
   ],
   providers: [],
   bootstrap: [AppComponent]
