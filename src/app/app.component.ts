75c06c9b92a3b43d6864174b14ca492d533b8259
Eduardo Moura <emoufilho@gmail.com>
2023-11-11 19:48:50 -0300

    Commit Secao 7

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -9,15 +9,37 @@ import { Component, OnInit } from '@angular/core';
     <h3>Final da Aula - ng-content </h3>
   </app-diretivas-atributos>
   <router-outlet></router-outlet>
-  <app-diretivas-atributos></app-diretivas-atributos>-->
+  <app-diretivas-atributos></app-diretivas-atributos>
   <app-new-component></app-new-component>
+  <app-input [contador]="addValue"></app-input>
+  <br>
+  <button (click)="add()">Adicionar</button>-->
+  <ng-template [ngIf]="getDados">
+    <h1>Nome: {{getDados.nome}}</h1>
+    <h1>Idade: {{getDados.idade}}</h1>
+  </ng-template>
+  <app-output (enviarDados)="setDados($event)"></app-output>
+  <router-outlet></router-outlet>
   `
 })
 
 export class AppComponent implements OnInit {
 
+  public addValue: number = 1;
+
+  public getDados: {nome: string, idade: number} | undefined;
+
   constructor() { }
 
   ngOnInit(): void { }
+
+  public add() {
+    this.addValue++;
+  } 
+
+  public setDados(event: {nome: string, idade: number}) {
+    this.getDados = event;
+    console.log(event)
+  }
 
 }
